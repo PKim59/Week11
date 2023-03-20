44d18ad6074cbff18074edae3c6aed0528dde6a6
Paul Kim Set B <pkim59@my.bcit.ca>
2023-03-20 10:04:39 -0700

    prompt and alert practice with functions complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,29 @@
-function greet(name, time = "Personnel") {
-    console.log("Nothing " + time + ", " + name)
+// function greet(name, time = "Personnel") {
+//     console.log("Nothing " + time + ", " + name)
+// }
+
+// greet("kiddo", "for you", "testttttt")
+
+// // third parameter is lost as it isn't needed or expected.
+
+
+// let func1 = function test1() {
+//     abc = prompt("Please enter your name!")
+//     console.log("What's up, " + abc)
+// }
+
+// func1()
+
+
+uservalue = prompt("Please enter the radius of the circle!")
+
+function circletime(uservalue) {
+    if (isNaN(uservalue))
+        alert("this isn't a number!");
+    else
+        area = (uservalue * uservalue) * Math.PI;
+    return area;
 }
 
-greet("kiddo")
-
-
-let func1 = function test1() {
-    abc = prompt("Please enter your name!")
-    console.log("What's up, " + abc)
-}
-
-func1()
-
-
+circletime(uservalue)
+alert("The area of a circle with a radius of " + uservalue + " is " + area.toFixed(2) + "! (full value = " + area + ")") 

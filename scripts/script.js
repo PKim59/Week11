9e0c5e870e96f652cd35468c1a640151c499c8fa
Paul Kim Set B <pkim59@my.bcit.ca>
2023-03-20 09:40:30 -0700

    Default values for functions!

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,15 @@
-function greet(name) {
-    console.log("Hello " + name)
+function greet(name, time = "Personnel") {
+    console.log("Nothing " + time + ", " + name)
 }
 
-greet("John")
+greet("kiddo")
+
 
 let func1 = function test1() {
     abc = prompt("Please enter your name!")
     console.log("What's up, " + abc)
 }
 
-test1()
+func1()
+
+
